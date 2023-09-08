bc9a779c4b3e51ce46c7262c3577fc1912244be0
Rose Kimball <65327579+RoseKimball@users.noreply.github.com>
2023-09-07 18:50:56 -0700

    mui table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,59 @@
-import Image from 'next/image'
+"use client";
+import { DataGrid, GridColDef, GridToolbarContainer, GridToolbarFilterButton } from '@mui/x-data-grid';
+import Box from '@mui/material/Box';
+import Data from '../../public/product-fixtures.json';
+
 
 export default function Home() {
+  console.log('data', Data)
+  const columns: GridColDef[] = [
+    { field: 'name', headerName: 'Name', minWidth: 250, align: 'left', headerAlign: 'left', filterable: false},
+    { field: 'color', headerName: 'Color', width: 130, flex: 1, align: 'center', headerAlign: 'center'},
+    { field: 'type', headerName: 'Type', width: 130, flex: 1, align: 'center', headerAlign: 'center', filterable: false },
+    {
+      field: 'cost',
+      headerName: 'Cost',
+      type: 'number',
+      width: 90,
+      align: 'right',
+      headerAlign: 'right',
+      filterable: false
+    }
+  ];
+  
+  const rows = Data.map((item) => ({
+    id: item.id,
+    name: item.name,
+    color: item.color,
+    type: item.type,
+    cost: item.price
+
+  }))
+
+  function CustomToolbar() {
+    return (
+      <GridToolbarContainer sx={{ marginLeft: 'auto' }}>
+        <GridToolbarFilterButton />
+      </GridToolbarContainer>
+    );
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>hello</div>
-    </main>
+    <Box sx={{ height: 'auto', width: '100%' }}>
+       <DataGrid
+          rows={rows}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 5 },
+            },
+          }}
+          pageSizeOptions={[5, 10]}
+          sx={{ m: 4 }}
+          slots={{
+            toolbar: CustomToolbar,
+          }}
+      />
+    </Box>
   )
 }
